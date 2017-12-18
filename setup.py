4ed67996a8fd1084e04b9cae87065516c5b63363
han8909227 <hbao2016@hotmail.com>
2017-12-18 08:16:39 -0800

    added setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""Set up file, to install this module : pip install -e ."""
+from setuptools import setup
+
+setup(
+    name='Avalara Python SDK',
+    package_dir={'': 'src'},
+    py_modules=['client'],
+    author='Han Bao, Adrienne Karnoski, Robert Bronson, Philip Werner',
+    author_email='hbao2016@hotmail.com',
+    description='Avalara Tax Python SDK.',
+    install_requires=[],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "tox"]
+    })
