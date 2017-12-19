728123236b9bffc7280b753481bbd16026773c74
han8909227 <hbao2016@hotmail.com>
2017-12-18 21:38:16 -0800

    added two more tests to complete today's test amount

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,6 @@
-"""Test the authorization."""
+"""Test the add_credential method."""
 import pytest
+from requests.auth import HTTPBasicAuth
 
 
 test_creds = {
@@ -44,7 +45,19 @@ def test_bearer_token_auth(unauth_client):
     assert unauth_client.auth == 'Bearer 123321'
 
 
+def test_auth_object(unauth_client):
+    """Test auth is HTTPBasicAuth object."""
+    unauth_client.add_credentials(test_creds)
+    assert isinstance(unauth_client.auth, HTTPBasicAuth)
+
+
 def test_auth_raises_value_error(unauth_client):
     """Test value error is raised with imporper authorization."""
     with pytest.raises(ValueError):
         unauth_client.add_credentials({})
+
+
+def test_no_auth_if_no_cred_is_added(unauth_client):
+    """Test client object auth property is None by default."""
+    assert unauth_client.auth is None
+
