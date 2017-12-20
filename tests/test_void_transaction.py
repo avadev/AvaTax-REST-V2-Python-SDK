83643dadf18a38e5361ad2104d51fa3275f73b41
Rob Bronson <robert.j.bronson@gmail.com>
2017-12-19 17:26:14 -0800

    fixed linter error

--- tests/test_void_transaction.py
+++ tests/test_void_transaction.py
@@ -10,7 +10,8 @@ def test_imports(single_transaction, auth_client):
 
 def test_void_changes_transaction_status_to(single_transaction, auth_client):
     """Test that running void_transaction voids it."""
-    result = auth_client.void_transaction('DEFAULT', single_transaction, {'code': 'DocVoided'}).json()
+    result = auth_client.void_transaction('DEFAULT', single_transaction,
+                                          {'code': 'DocVoided'}).json()
     assert result['status'] == "Cancelled"
 
 
@@ -20,13 +21,15 @@ def test_void_without_model_info_raises_error(single_transaction, auth_client):
         auth_client.void_transaction("DEFAULT", single_transaction)
 
 
-def test_void_without_company_code_raises_errror(single_transaction, auth_client):
+def test_void_without_company_code_raises_errror(single_transaction,
+                                                 auth_client):
     """Test error is raised when called without all params."""
     with pytest.raises(ValueError):
         auth_client.void_transaction(single_transaction, {'code': 'DocVoided'})
 
 
-def test_void_without_transaction_num_raises_error(single_transaction, auth_client):
+def test_void_without_transaction_num_raises_error(single_transaction,
+                                                   auth_client):
     """Test error is raised when called without all params."""
     with pytest.raises(ValueError):
         auth_client.void_transaction('DEFAULT', {'code': 'DocVoided'})
