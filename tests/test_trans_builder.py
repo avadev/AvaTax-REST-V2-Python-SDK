bafd700020583323de8fc59b09bd1e8604bb273b
han8909227 <hbao2016@hotmail.com>
2017-12-21 11:12:40 -0800

    testting functionality of transaction builder

---
+++ tests/test_trans_builder.py
@@ -0,0 +1,116 @@
+"""Test Transactin Builder class."""
+from transaction_builder import TransactionBuilder
+
+
+def test_initialize_builder_object(auth_client):
+    """Test if a new transaction can be initialize."""
+    new_trans = TransactionBuilder(auth_client, 'DEFAULT', 'SalesInvoice', 'ABC123')
+    assert isinstance(new_trans, TransactionBuilder)
+
+
+def test_with_commit_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_commit()
+    assert trans.create_model['commit'] is True
+
+
+def test_with_transaction_code_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_transaction_code('1234567')
+    assert trans.create_model['code'] == '1234567'
+
+
+def test_with_type_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_type('SalesInvoice')
+    assert trans.create_model['type'] == 'SalesInvoice'
+
+
+def test_with_address_method(mt_trans, valid_address):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_address('SingleLocation', valid_address)
+    assert trans.create_model['addresses']['SingleLocation'] == valid_address
+
+
+def test_with_line_address_method(mt_trans, valid_address):
+    """Test method functionality of the Transaction Builder."""
+    mt_trans.with_line_address('SingleLocation', valid_address)
+    last_line = mt_trans.get_most_recent_line()
+    assert last_line['addresses']['SingleLocation'] == valid_address
+
+
+def test_with_latlong_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    lat_long = {'latitude': 47.6187, 'longitude': -122.3524}
+    trans = mt_trans.with_latlong('SingleLocation', 47.6187, -122.3524)
+    assert trans.create_model['addresses']['SingleLocation'] == lat_long
+
+
+def test_with_line_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    temp = {
+        'amount': 20,
+        'number': '1',
+        'quantity': 100,
+        'itemCode': 'ITEM2001',
+        'taxCode': '1234567'
+    }
+    trans = mt_trans.with_line(20, 100, 'ITEM2001', 1234567)
+    assert trans.create_model['lines'][-1] == temp
+
+
+def test_with_exempt_line_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    temp = {
+        'amount': 20,
+        'number': '1',
+        'quantity': 1,
+        'itemCode': 'ITEM2001',
+        'exemptionCode': '1234567'
+    }
+    trans = mt_trans.with_exempt_line(20, 'ITEM2001', 1234567)
+    assert trans.create_model['lines'][-1] == temp
+
+
+def test_with_diagnostics_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_diagnostics()
+    assert trans.create_model['debugLevel'] == 'Diagnostic'
+
+
+def test_with_discount_amount_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_discount_amount(200)
+    assert trans.create_model['discount'] == 200
+
+
+def test_with_parameter_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_parameter('param', 'param_val')
+    assert trans.create_model['parameters']['param'] == 'param_val'
+
+
+def test_with_line_parameter_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_parameter('param', 'param_val')
+    assert trans.create_model['parameters']['param'] == 'param_val'
+
+
+def test_get_most_recent_line_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    line = mt_trans.get_most_recent_line('TestName')
+    assert line is mt_trans.create_model['lines'][-1]
+
+
+def test_with_line_tax_overrride_method(mt_trans):
+    """Test method functionality of the Transaction Builder."""
+    trans = mt_trans.with_parameter('param', 'param_val')
+    assert trans.create_model['parameters']['param'] == 'param_val'
+
+
+
+
+
+
+
+
