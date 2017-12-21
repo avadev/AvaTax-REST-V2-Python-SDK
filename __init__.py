c3296ee1d0dd9a69ebcbded8162514715dcb3438
han8909227 <hbao2016@hotmail.com>
2017-12-21 11:13:02 -0800

    testting functionality of transaction builder

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""Return proper type checking tuple based on Python version of current environment."""
-import sys
-
-
-if sys.version_info.major == 3:
-    str_type = (str, type(None))
-else:
-    str_type = (str, unicode, type(None))
