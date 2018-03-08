982ecbc484a03322d97bd5a18b87d4f7a8a8656b
Rob Bronson Avalara <robert.bronson@avalara.com>
2018-03-08 11:46:41 -0800

    Removed trouble zip code tracking, truncated zip list to 500 for testing, set additional tax codes as requested.  Ready to test.

--- src/sendsales.py
+++ src/sendsales.py
@@ -24,36 +24,57 @@ import pdb
 user_auth = (os.environ["USERNAME"], os.environ["PASSWORD"])
 codes = requests.get('https://sandbox-rest.avatax.com/api/v2/definitions/postalcodes',
                      auth=user_auth)
+test_codes = codes.json()["value"][0:500] #small sample of codes to generate reports with to make sure its working
+
 loc_list = []
-tax_codes = ["P0000000"]
+tax_codes = ["P0000000", "FR020100", "SF096370"]
 id_count = 1
 total_start_time = time.time()
-for entry in codes.json()["value"]:
+for entry in test_codes:   #codes.json()["value"]:
     # call CreateLocations on each one
     duration = 0
     start = time.time()
     addr_data = {"Address": {"PostalCode": "{}".format(entry["postalCode"])}}
     temp_address = requests.post("http://dev.address-service.avalara.net/AddressService/api/address/resolve",
                                  json=addr_data)
-    # temp_address.json()["NormalizedAddress"]
+    if "NormalizedAddress" not in temp_address.json():
+        continue
+    else:
+        addr = temp_address.json()["NormalizedAddress"]
     duration += time.time() - start
 
+    # loc = [{"id": id_count,
+    #         "locationCode": "Location {}".format(id_count),
+    #         "addressTypeId": "Location",
+    #         "addressCategoryId": "Warehouse",
+    #         "line1": temp_address.json()["NormalizedAddress"]["Line1"],
+    #         "city": temp_address.json()["NormalizedAddress"]["City"],
+    #         "county": temp_address.json()["NormalizedAddress"]["County"],
+    #         "region": temp_address.json()["NormalizedAddress"]["State"],
+    #         "postalCode": "{}".format(entry["postalCode"]),
+    #         "country": "US"}]
     loc = [{"id": id_count,
             "locationCode": "Location {}".format(id_count),
             "addressTypeId": "Location",
             "addressCategoryId": "Warehouse",
             "line1": temp_address.json()["NormalizedAddress"]["Line1"],
-            # "city": temp_address.json()["NormalizedAddress"]["City"],
-            "county": temp_address.json()["NormalizedAddress"]["County"],
-            "region": temp_address.json()["NormalizedAddress"]["State"],
             "postalCode": "{}".format(entry["postalCode"]),
             "country": "US"}]
+    if "City" in addr:
+        loc[0]["city"] = addr["City"]
+    else:
+        continue
+    if "State" in addr:
+        loc[0]["state"] = addr["State"]
+    else:
+        continue
+
     start = time.time()
     # pdb.set_trace()
     pdb.set_trace()
     new_loc = requests.post("https://sandbox-rest.avatax.com/api/v2/companies/{}/locations".format(os.environ["COMPANY"]),
                             auth=user_auth, json=loc)
-
+    print (new_loc.json())
     duration += time.time() - start
     # new_loc.json()[0]["id"]
     model = {
@@ -62,11 +83,11 @@ for entry in codes.json()["value"]:
         "taxCodes": tax_codes
     }
     start = time.time()
+    # call BuildTaxContentFile() for each location
     tax_file = requests.post("https://sandbox-rest.avatax.com/api/v2/pointofsaledata/build", auth=user_auth, json=model)
     duration += time.time() - start
-    # call BuildTaxContentFile() for each location
     # collect total time elapsed.
-    loc_list.append([tax_file, duration])
+    loc_list.append([tax_file, duration]). < ----- UNCOMMENT THIS !!!!!!
     id_count += 1
 final_time = time.time() - total_start_time
 
