32e7dfc3ba28b6d810734e4f8e13c69ad885835a
HAN BAO <hbao2016@hotmail.com>
2018-03-14 17:17:48 -0700

    more test added to basic workflow

--- tests/test_basic_workflow.py
+++ tests/test_basic_workflow.py
@@ -3,13 +3,7 @@ Ensure all methods needed for new user are functional, including going through
 the Transaction State Diagram to verify all methods needed for the lifecycle of 
 a single transaction works properly.
 """
-from faker import Faker
 import pytest
-
-client_info = {
-	'company_name': None,
-
-}
 
 
 @pytest.mark.incremental  # allow "test-step" : only continue to next test if previous ones all pass
@@ -22,20 +16,27 @@ class TestBasicWorkFlow(object):
 		"""Test credential is valid."""
 		assert '"authenticated":true' in auth_client.ping().text
 
-	def test_company_initialization(self, auth_client, init_comp_model):
-		"""Test a basic company can be initalized."""
-		fake = Faker()
-		comp_name = fake.first_name().upper()  # create new company each time test is run to prevent conflict
-		model = init_comp_model
-		model['companyCode'] = comp_name
-		response = auth_client.company_initialize(model)
-		assert response.status_code == 201
-		if response.status_code == 201:
+	def test_check_default_company_exist(self, auth_client):
+		"""Test if the user has default company set up already, create one otherwise."""
+		response = auth_client.query_companies()
+		assert response.status_code == 200
+		if '"companyCode":"DEFAULT"' not in response.text:
+			response = auth_client.company_initialize(init_comp_model)
+			assert response.status_code == 201
 			assert response.json()['isActive'] == True
-			client_info['company_name'] = comp_name
 
-	def test_create_transaction(auth_client):
-		"""Create a transaction using the company just created"""
-		pass
+	def test_create_transaction(self, auth_client, tax_document):
+		"""Create a transaction(save it only) using the company created(DEFAULT)."""
+		tax_document['commit'] = False
+		response = auth_client.create_transaction(tax_document, None)
+		assert response.json()['status'] == 'Saved'
+		auth_client.trans_code = response.json()['code']
+
+	def test_verfiy_transaction(self, auth_client):
+		"""Verify the transaction that has been created."""
+		response = auth_client.verify_transaction('DEFAULT', auth_client.trans_code, {})
+		assert response.json()['status'] == 'Posted'
+
+
 
 
