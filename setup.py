2b66ef4e4348d4c668a3623b7d14463fd9c86dbe
HAN BAO <hbao2016@hotmail.com>
2018-06-06 14:54:17 -0700

    adding long description in the setup.py to be consumed by PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 ﻿"""Set up file, to install this module : pip install -e ."""
 from setuptools import setup
 
+
+pkg_description = ''
+with open('README.md') as f:
+    pkg_description = file.read(f)
+
+
 setup(
     name='Avalara',
-    version='18.5.1',
+    version='18.5.2',
     url='https://github.com/avadev/AvaTax-REST-V2-Python-SDK',
     package_dir={'': 'src'},
     py_modules=[
@@ -16,6 +22,8 @@ setup(
     author='Han Bao, Adrienne Karnoski, Robert Bronson, Philip Werner',
     author_email='han.bao@avalara.com',
     description='Avalara Tax Python SDK.',
+    long_description=pkg_description,
+    long_description_content_type='text/markdown',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
