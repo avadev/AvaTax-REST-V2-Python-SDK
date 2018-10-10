c4e272ff5ac962fb711f4f85169f4ccd837aa43b
contygm <contygm@gmail.com>
2018-10-10 16:34:34 -0700

    ran update exe

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='Avalara',
-    version='18.5.2',
+    version='18.9.0',
     url='https://github.com/avadev/AvaTax-REST-V2-Python-SDK',
     package_dir={'': 'src'},
     py_modules=[
