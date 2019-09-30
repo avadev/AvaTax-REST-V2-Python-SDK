6620fafe74d4f173ed8b4d74329cde4d5c65b43f
Chris Walker <chris.walker@avalara.com>
2019-09-30 11:24:18 -0700

    19.9.0 update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='Avalara',
-    version='19.8.0',
+    version='19.9.0',
     url='https://github.com/avadev/AvaTax-REST-V2-Python-SDK',
     package_dir={'': 'src'},
     py_modules=[
