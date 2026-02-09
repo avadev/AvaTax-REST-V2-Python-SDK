187b61c9cc73fbc0bfb8bb2ba10b843f3257871e
svc-developer <svc-developer@avalara.com>
2026-02-09 17:44:52 +0000

    26.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='Avalara',
-    version='25.12.0',
+    version='26.2.0',
     url='https://github.com/avadev/AvaTax-REST-V2-Python-SDK',
     package_dir={'': 'src'},
     packages=['avalara'],
